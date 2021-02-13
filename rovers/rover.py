7f811e5490e89ea802de733232cb0d41aeee0378
Dillon <dmontag23@gmail.com>
2021-02-13 01:24:00 +0100

    Add rover class with unit tests

---
+++ rovers/rover.py
@@ -0,0 +1,76 @@
+class Rover(object):
+    """
+    Creates a Rover object.
+    """
+
+    # stores the possible values of the variables that describe the rover's movements
+    DIRECTIONS = ('N', 'E', 'S', 'W')
+    MOVEMENTS = ('L', 'R', 'M')
+
+    def __init__(self, initial_position, initial_direction, terrain):
+        """
+        Initialize a rover
+        """
+        self._position = initial_position
+        self._direction = initial_direction
+        self._terrain = terrain
+
+    def current_position(self):
+        return str(self._position.x) + " " + str(self._position.y) + " " + self._direction
+
+    @property
+    def direction(self):
+        return self._direction
+
+    @direction.setter
+    def direction(self, new_direction):
+        if self.is_valid_direction(new_direction):
+            self._direction = new_direction
+        else:
+            raise Exception(new_direction + " is not a valid direction!")
+
+    def execute_command(self, command):
+        for i in range(len(command)):
+            self.execute_movement(command[i])
+
+    def execute_movement(self, movement):
+        if not self.is_valid_movement(movement):
+            raise Exception(movement + " is not a valid movement!")
+        if movement == 'L':
+            self._direction = self.DIRECTIONS[(self.DIRECTIONS.index(self._direction) - 1) % len(self.DIRECTIONS)]
+        elif movement == 'R':
+            self._direction = self.DIRECTIONS[(self.DIRECTIONS.index(self._direction) + 1) % len(self.DIRECTIONS)]
+        elif movement == 'M':
+            self.move()
+
+    def is_valid_direction(self, input_dir):
+        return input_dir in self.DIRECTIONS
+
+    def is_valid_movement(self, input_move):
+        return input_move in self.MOVEMENTS
+
+    def move(self):
+        if self._direction == 'N':
+            self._position.y += 1
+        elif self._direction == 'E':
+            self._position.x += 1
+        elif self._direction == 'S':
+            self._position.y -= 1
+        elif self._direction == 'W':
+            self._position.x -= 1
+        if not self._terrain.is_valid_move(self._position):
+            raise Exception("The rover moved off the plateau and burst into flame! (You tried to move to: ("
+                            + str(self._position.x) + " " + str(self._position.y) +"))")
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, new_position):
+        if self._terrain.is_valid_move(new_position):
+            self._position = new_position
+        else:
+            raise Exception("You realize the position ("
+                            + str(new_position.x) + " " + str(new_position.y) +") is off the plateau right?")
+
---
+++ test/test_rover.py
@@ -0,0 +1,125 @@
+import unittest
+from rovers.point import Point
+from rovers.rover import Rover
+from rovers.terrain import Terrain
+
+class TestRover(unittest.TestCase):
+    '''
+    Tests the Rover class.
+    '''
+
+    def setUp(self):
+        self._test_initial_point = Point(0,0)
+        self._test_point = Point(10, 10)
+        self._test_direction = 'E'
+        self._test_terrain = Terrain(self._test_point)
+        self._test_rover = Rover(self._test_initial_point, self._test_direction, self._test_terrain)
+
+    def test_current_position(self):
+        self.assertEqual(str(self._test_initial_point.x) + " " + str(self._test_initial_point.y) + " " + self._test_direction,
+                         self._test_rover.current_position())
+
+    def test_direction_getter(self):
+        self.assertEqual(self._test_direction, self._test_rover.direction)
+
+    def test_direction_setter(self):
+        new_value = 'N'
+        self._test_rover.direction = new_value
+        self.assertEqual(new_value, self._test_rover.direction)
+
+        with self.assertRaises(Exception) as context:
+            new_value = 'n'
+            self._test_rover.direction = new_value
+        self.assertTrue(new_value + ' is not a valid direction!' in str(context.exception))
+
+    def test_execute_command(self):
+        self._test_rover.execute_command("LMRRMLMLM")
+        self.assertEqual(Point(1,1), self._test_rover.position)
+        self.assertEqual('N', self._test_rover.direction)
+
+    def test_execute_movement(self):
+        directions = self._test_rover.DIRECTIONS
+        self.assertEqual(self._test_direction, self._test_rover.direction)
+        new_direction = directions[(directions.index(self._test_rover.direction) + 1) % len(directions)]
+        self._test_rover.execute_movement('R')
+        self.assertEqual(new_direction, self._test_rover.direction)
+
+        new_direction = directions[(directions.index(self._test_rover.direction) - 1) % len(directions)]
+        self._test_rover.execute_movement('L')
+        self.assertEqual(new_direction, self._test_rover.direction)
+
+        turns = 6
+        new_direction = directions[(directions.index(self._test_rover.direction) + turns) % len(directions)]
+        for i in range(turns):
+            self._test_rover.execute_movement('R')
+        self.assertEqual(new_direction, self._test_rover.direction)
+
+        turns = 6
+        new_direction = directions[(directions.index(self._test_rover.direction) - turns) % len(directions)]
+        for i in range(turns):
+            self._test_rover.execute_movement('L')
+        self.assertEqual(new_direction, self._test_rover.direction)
+
+        with self.assertRaises(Exception) as context:
+            new_movement = 'n'
+            self._test_rover.execute_movement(new_movement)
+        self.assertTrue(new_movement + ' is not a valid movement!' in str(context.exception))
+
+    def test_is_valid_direction(self):
+        self.assertTrue(self._test_rover.is_valid_direction('N'))
+        self.assertTrue(self._test_rover.is_valid_direction('E'))
+        self.assertTrue(self._test_rover.is_valid_direction('S'))
+        self.assertTrue(self._test_rover.is_valid_direction('W'))
+        self.assertFalse(self._test_rover.is_valid_direction('n'))
+        self.assertFalse(self._test_rover.is_valid_direction('4'))
+        self.assertFalse(self._test_rover.is_valid_direction('432.3'))
+
+    def test_is_valid_movement(self):
+        self.assertTrue(self._test_rover.is_valid_movement('L'))
+        self.assertTrue(self._test_rover.is_valid_movement('R'))
+        self.assertTrue(self._test_rover.is_valid_movement('M'))
+        self.assertFalse(self._test_rover.is_valid_movement('l'))
+        self.assertFalse(self._test_rover.is_valid_movement('r'))
+        self.assertFalse(self._test_rover.is_valid_movement('42'))
+        self.assertFalse(self._test_rover.is_valid_movement('42.42'))
+
+    def test_move(self):
+        self._test_rover.move()
+        self.assertEqual(Point(1,0), self._test_rover.position)
+
+        self._test_rover.direction = 'N'
+        self._test_rover.move()
+        self.assertEqual(Point(1,1), self._test_rover.position)
+
+        self._test_rover.direction = 'W'
+        self._test_rover.move()
+        self.assertEqual(Point(0,1), self._test_rover.position)
+
+        self._test_rover.direction = 'S'
+        self._test_rover.move()
+        self.assertEqual(Point(0,0), self._test_rover.position)
+
+        with self.assertRaises(Exception) as context:
+            self._test_rover.move()
+        self.assertTrue("The rover moved off the plateau and burst into flame! (You tried to move to: ("
+                            + str(self._test_rover._position.x) + " " + str(self._test_rover._position.y) +"))"
+                        in str(context.exception))
+
+    def test_position_getter(self):
+        self.assertEqual(self._test_initial_point, self._test_rover.position)
+
+    def test_position_setter(self):
+        new_value = Point(4,3)
+        self._test_rover.position = new_value
+        self.assertEqual(new_value, self._test_rover.position)
+
+        with self.assertRaises(Exception) as context:
+            new_position = Point(self._test_point.x + 1, 0)
+            self._test_rover.position = new_position
+        self.assertTrue("You realize the position ("
+                            + str(new_position.x) + " " + str(new_position.y) +") is off the plateau right?"
+                        in str(context.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
